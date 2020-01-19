1899e931fa04483fd60f47f67b0df738d14fc6c9
Anmol Gupta <anmolify@gmail.com>
2020-01-19 22:10:38 +0530

    Conditionals Method1

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Person from './Person /Person';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person /Person";
 
 class App extends Component {
-
   state = {
-    persons : [
-      {name : 'Anmol Gupta', age : 22},
-      {name : 'Aastha Bubber', age: 23},
-      {name : 'Dumpy Gupta', age: 22}
-    ]
-  }
+    persons: [
+      { name: "Anmol Gupta", age: 22 },
+      { name: "Aastha Bubber", age: 23 },
+      { name: "Dumpy Gupta", age: 22 }
+    ],
+    showPersons: false
+  };
 
-  switchNameHandler = (newName) => {
+  switchNameHandler = newName => {
     // console.log("Was Clicked!");
     this.setState({
-      persons : [
-        {name : newName, age : 22},
-        {name : 'Aastha Bubber', age: 23},
-        {name : 'Ayushi Gupta', age: 26}
+      persons: [
+        { name: newName, age: 22 },
+        { name: "Aastha Bubber", age: 23 },
+        { name: "Ayushi Gupta", age: 26 }
       ]
-    })
-  }
+    });
+  };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = event => {
     this.setState({
-      persons : [
-        {name : event.target.value, age : 22},
-        {name : 'Aastha Bubber', age: 23},
-        {name : 'Ayushi Gupta', age: 26}
+      persons: [
+        { name: event.target.value, age: 22 },
+        { name: "Aastha Bubber", age: 23 },
+        { name: "Ayushi Gupta", age: 26 }
       ]
-    })
-  }
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  };
 
   render() {
-
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '2px groove blue',
-      padding: '8px'
+      backgroundColor: "white",
+      font: "inherit",
+      border: "2px groove blue",
+      padding: "8px"
     };
 
     return (
@@ -49,11 +55,28 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <button style={style} onClick={() => this.switchNameHandler('CoderAnmolGupta')}>Press Me</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} changed={this.nameChangedHandler}/>
-        {/* <Person name="Aastha Bubber" age="23">She is very intelligent and beautiful.</Person> */}
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this,'RichAnmolGupta')}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Press Me
+        </button>
+        {this.state.showPersons ?
+          (<div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              changed={this.nameChangedHandler}
+            />
+            {/* <Person name="Aastha Bubber" age="23">She is very intelligent and beautiful.</Person> */}
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, "RichAnmolGupta")}
+            />
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div>)
+        : null}
       </div>
     );
   }
